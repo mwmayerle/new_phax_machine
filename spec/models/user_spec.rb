4d35dbf4c8b528b9f52394ab35ea9cd6b9d368b9
Matt Mayerle <mwmayerle@gmail.com>
2018-06-01 09:31:07 -0500

    Initial commit. Added Phaxio, rspec-rails, phonelib, and databasecleaner gems

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe "a fake test for TravisCI" do
+  	it "returns true" do
+  		variable = true
+  		expect(variable).to be true
+  	end
+  end
+end
