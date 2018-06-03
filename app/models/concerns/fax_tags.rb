c341c771b03126f1c30995d2a33762423d9f738b
Matt Mayerle <mwmayerle@gmail.com>
2018-06-03 10:33:35 -0500

    Added super_user model validations and spec tests

--- app/models/concerns/fax_tags.rb
+++ app/models/concerns/fax_tags.rb
@@ -1,6 +1,8 @@
 module FaxTags
+
   def generate_fax_tag
-    return if fax_tag.present?
+    return if self.fax_tag.present?
     self.fax_tag = SecureRandom.uuid
   end
+  
 end
--- spec/models/super_user_spec.rb
+++ spec/models/super_user_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe SuperUser, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "creating a SuperUser with valid input" do
+  	before(:each) do
+    	@admin = Admin.create!(admin_email: 'tom@tom.com', password: 'tomtom')
+    	@super_user = SuperUser.new(admin_id: @admin.id, super_user_email: 'tom@tom.com', password: 'tomtom')
+  	end
+
+  	it "has an admin email" do
+  		expect(@super_user.valid?).to be true
+  		expect(@super_user.save).to be true
+  	end
+
+  	it "has a password" do
+  		expect(@super_user.valid?).to be true
+  		expect(@super_user.save).to be true
+  	end
+
+  	it "generates a fax_tag is none is supplied" do
+  		@super_user.save
+  		expect(@super_user.fax_tag).not_to be_nil
+  	end
+
+  	it "preserves a valid user-inputted fax tag" do
+  		@super_user.fax_tag = "Custom fax tag"
+  		@super_user.save
+  		expect(@super_user.fax_tag).to eq("Custom fax tag")
+  	end
+  end
+
+  describe "creating a SuperUser with invalid input" do
+  	before(:each) do
+    	@admin = Admin.create!(admin_email: 'tom@tom.com', password: 'tomtom')
+    	@super_user = SuperUser.new(admin_id: @admin.id, super_user_email: 'tom@tom.com', password: 'tomtom')
+  	end
+  	# learned that @super_user.admin_id = '1' will persist...
+  	it "does not persist if an admin_id is not present or is not a number" do
+  		@super_user.admin_id = 'one'
+  		expect(@super_user.valid?).to be false
+  		expect(@super_user.save).to be false
+  		@super_user.admin_id = nil
+  		expect(@super_user.valid?).to be false
+  		expect(@super_user.save).to be false
+  	end
+
+  	it "does not persist if the fax_tag is longer than 60 characters" do
+  		@super_user.fax_tag = "A" * 64
+  		expect(@super_user.valid?).to be false
+  		expect(@super_user.save).to be false
+  	end
+
+  	it "does not persist if the super_user already exists" do
+  		@super_user.save
+  		@super_user2 = SuperUser.new(admin_id: @admin.id, super_user_email: 'tom@tom.com', password: 'tomtom')
+  		expect(@super_user2.valid?).to be false
+  		expect(@super_user2.save).to be false
+  	end
+  end
 end
