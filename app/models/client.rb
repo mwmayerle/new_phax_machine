44d43bfdf84bdc0e2337fe4cd348156c2e1aad1f
Matt Mayerle <mwmayerle@gmail.com>
2018-06-14 11:35:50 -0500

    solved client deletion bug that left orphaned objects

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,12 +4,12 @@ class Client < ApplicationRecord  #LENGTH_LIMIT constant is in application_recor
 	attr_readonly :admin_id
 
 	belongs_to :admin, class_name: :User
-	belongs_to :client_manager, optional: true
+	belongs_to :client_manager, optional: true, dependent: :destroy
 
 	has_many :fax_numbers
 	has_many :emails
 	has_many :fax_number_emails, through: :emails
-	has_many :users
+	has_many :users, dependent: :destroy
 
 	validates :admin_id, presence: true, numericality: { integer_only: true }
 	validates :client_manager_id, numericality: { integer_only: true, allow_blank: true }
@@ -17,12 +17,14 @@ class Client < ApplicationRecord  #LENGTH_LIMIT constant is in application_recor
 	validates :fax_tag, uniqueness: true, length: { maximum: 60 }, presence: true
 
 	before_validation :generate_fax_tag
-	# before_destroy :delete_associated
+	before_destroy :modify_and_delete_associated
 
 	private
-		# def delete_associated
-		# 	FaxNumber.where(client_id: self.id).destroy_all
-		# 	emails = Email.where(client_id: self.id).destroy_all
-		# 	FaxNumberEmail.where(email_id: emails).destroy_all
-		# end
+		def modify_and_delete_associated
+			FaxNumber.where(client_id: self.id).each do |fax_number|
+				fax_number.update_attributes( { client_id: nil, fax_number_display_label: nil, fax_number_label: nil } )
+			end
+			emails = Email.where(client_id: self.id).destroy_all
+			FaxNumberEmail.where(email_id: emails).destroy_all
+		end
 end
