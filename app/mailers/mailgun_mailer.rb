9906d20100ff2e608dff3bbf9d866de8e07a9b49
Matt Mayerle <mwmayerle@gmail.com>
2018-06-26 11:40:11 -0500

    Reconfigured mailer to get it out of Devise's dictatorship

--- app/mailers/mailgun_mailer.rb
+++ app/mailers/mailgun_mailer.rb
@@ -2,10 +2,10 @@ class MailgunMailer < ApplicationMailer
 	def fax_sent(sender, client)
   	@sender = sender
   	@client = client
-  	
   	mail(
   		to: @sender.email_address,
-  		from: ENV["FROM_EMAIL"]
+  		from: ENV["FROM_EMAIL"],
+  		template: 'fax_sent',
   	)
   end
 
