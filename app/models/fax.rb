b2008422c18a7f051401123b552e80e6ad46f1a3
Matt Mayerle <mwmayerle@gmail.com>
2018-06-27 17:50:41 -0500

    fleshing out send_fax method for sending a fax from email to the domain

--- app/models/fax.rb
+++ app/models/fax.rb
@@ -9,20 +9,20 @@ class Fax
 	  	errors.max_by {|error_code, amount| amount["frequency"]}.shift
 		end
 
-		def send_fax(sender, params+recip, files)
+		def send_fax(sender, params_recip, files)
 			p "**********************************************************"
 			set_phaxio_creds
 
-			begin
-        user_id = db[:user_emails].where do |user|
-          {user.lower(:email) => fromEmail&.downcase}
-        end.first[:user_id]
-        user = db[:users].where(id: user_id).first
-        from_fax_number = user[:fax_number]
-        fax_tag = user[:fax_tag]
-      ensure
-        db.disconnect
-      end
+			# begin
+   #      user_id = db[:user_emails].where do |user|
+   #        {user.lower(:email) => fromEmail&.downcase}
+   #      end.first[:user_id]
+   #      user = db[:users].where(id: user_id).first
+   #      from_fax_number = user[:fax_number]
+   #      fax_tag = user[:fax_tag]
+   #    ensure
+   #      db.disconnect
+   #    end
 
       number = Mail::Address.new(toEmail).local
       options = {to: number, caller_id: from_fax_number, :"tag[user]" => fax_tag}
@@ -31,25 +31,25 @@ class Fax
 
       logger.info "#{fromEmail} is attempting to send #{filenames.length} files to #{number}..."
       result = Phaxio.send_fax(options)
-      result = JSON.parse result.body
+      result = JSON.parse(result.body)
 
       if result['success']
         logger.info "Fax queued up successfully: ID #" + result['data']['faxId'].to_s
       else
         logger.warn "Problem submitting fax: " + result['message']
 
-        if ENV['SMTP_HOST']
-          #send mail back to the user telling them there was a problem
+        # if ENV['SMTP_HOST']
+        #   #send mail back to the user telling them there was a problem
 
-          Pony.mail(
-            :to => fromEmail,
-            :from => smtp_from_address,
-            :subject => 'Mailfax: There was a problem sending your fax',
-            :body => "There was a problem faxing your #{filenames.length} files to #{number}: " + result['message'],
-            :via => :smtp,
-            :via_options => smtp_options
-          )
-        end
+        #   Pony.mail(
+        #     :to => fromEmail,
+        #     :from => smtp_from_address,
+        #     :subject => 'Mailfax: There was a problem sending your fax',
+        #     :body => "There was a problem faxing your #{filenames.length} files to #{number}: " + result['message'],
+        #     :via => :smtp,
+        #     :via_options => smtp_options
+        #   )
+        # end
       end
 		end
 	end
