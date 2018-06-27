90017eb7972530407048e1e2398c354edf8d93f9
Matt Mayerle <mwmayerle@gmail.com>
2018-06-27 14:47:36 -0500

    debugging for file attachment in Mailgun

--- app/controllers/mailgun_faxes_controller.rb
+++ app/controllers/mailgun_faxes_controller.rb
@@ -78,6 +78,6 @@ class MailgunFaxesController < ApplicationController
 	private
 		def mailgun_params
 			params.require('fax').permit('id', 'num_pages', 'cost', 'direction', 'status', 'is_test', 'requested_at', 'completed_at', { 'recipients':
-				['number', 'status', 'bitrate', 'resolution', 'completed_at']}, { 'tags': ['sender_client_fax_tag', 'sender_fax_tag'] }, 'filename')
+				['number', 'status', 'bitrate', 'resolution', 'completed_at']}, { 'tags': ['sender_client_fax_tag', 'sender_fax_tag'] }).permit!('filename')
 		end
 end
