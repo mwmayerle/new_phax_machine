b1965a55d8bf37ea9fa14cbac8098d4eda416cff
Matt Mayerle <mwmayerle@gmail.com>
2018-06-28 14:22:16 -0500

    Added text and subjects to rejection email

--- app/controllers/mailgun_faxes_controller.rb
+++ app/controllers/mailgun_faxes_controller.rb
@@ -47,7 +47,7 @@ class MailgunFaxesController < ApplicationController
 
     sender = Mail::AddressList.new(params['from']).addresses.first.address
  		api_response = Fax.create_fax_from_email(sender, params['recipient'], files)
- 		puts api_response
+ 		puts api_response.get
  		if api_response['status'] != 'queued'
 	 		MailgunMailer.email_to_fax_failed(sender, files, api_response['fax']['recipients']).deliver_now
 	 	end
