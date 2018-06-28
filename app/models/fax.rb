18ac73aa037a9e5c869b7c5ad0c4ad5785d98ae4
Matt Mayerle <mwmayerle@gmail.com>
2018-06-28 13:21:33 -0500

    added object back into mailgun method

--- app/models/fax.rb
+++ app/models/fax.rb
@@ -16,9 +16,11 @@ class Fax
 			)
 		end
 
-		def send_fax_from_email(sender, recipient, files)
+		def send_fax_from_email(user_email, recipient, files)
 			set_phaxio_creds
       number = Mail::Address.new(recipient).local
+      user_email = UserEmail.find_by(email_address: sender)
+
 
       options = {
       	to: number,
