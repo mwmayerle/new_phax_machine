c3a73cc2210477aebd6cd293b76e49c8d5c29309
Matt Mayerle <mwmayerle@gmail.com>
2018-07-03 15:57:40 -0500

    Heroku error handling

--- app/controllers/mailgun_faxes_controller.rb
+++ app/controllers/mailgun_faxes_controller.rb
@@ -60,25 +60,3 @@ class MailgunFaxesController < ApplicationController
 		end
 	end
 end
-
-	def create(attached_files = [])
-		fax_params[:files].each { |file_in_params| attached_files << file_in_params[1].tempfile } # No .map() for ActionCont. params
-		options = {
-			to: fax_params[:to],
-			files: attached_files,
-			caller_id: current_user.user_email.caller_id_number,
-			tag: { 
-				sender_client_fax_tag: current_user.client.fax_tag, 
-				sender_email_fax_tag: current_user.user_email.fax_tag,
-			},
-		}
-		sent_fax_response = Fax.create_fax(options)
-		# in Fax.create_fax I rescue Phaxio::Error-type errors and convert them into a string to catch garbage input
-		if sent_fax_response.class == String
-			flash[:alert] = sent_fax_response
-		else
-			api_response = Fax.get_fax_information(sent_fax_response)
-			api_response.status == 'queued' ? flash[:notice] = 'Fax queued for sending' : flash[:alert] = api_response.error_message
-		end
-		redirect_to new_fax_path
-	end
