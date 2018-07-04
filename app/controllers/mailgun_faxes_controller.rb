02cb6adfee832f895331479028558b019ba6f0d3
Matt Mayerle <mwmayerle@gmail.com>
2018-07-04 10:32:26 -0500

    callback token debugging

--- app/controllers/mailgun_faxes_controller.rb
+++ app/controllers/mailgun_faxes_controller.rb
@@ -2,14 +2,11 @@ class MailgunFaxesController < ApplicationController
 	skip_before_action :verify_authenticity_token, only: [:fax_received, :fax_sent, :mailgun]
 
 	def fax_received
-		p "================================================================"
-		p params
-		p "================================================================"
-		@fax = JSON.parse(params['fax'])
-		p "================================================================"
-		p request.headers.each {|header| puts header}
-		p "================================================================"
-		p response.headers.each {|header| puts header}
+		p "============================================================"
+		p phaxio_signature = response.headers.HTTP_X_PHAXIO_SIGNATURE
+		p phaxio_signature2 = response.headers.HTTP_X_PHAXIO_SIGNATURE2
+		p "============================================================"
+
 		@fax = JSON.parse(params['fax'])
     recipient_number = Phonelib.parse(@fax['to_number']).e164
     fax_number = FaxNumber.find_by(fax_number: recipient_number)
@@ -27,14 +24,11 @@ class MailgunFaxesController < ApplicationController
 	end
 
 	def fax_sent
-		p "================================================================"
-		p params
-		p "================================================================"
-		@fax = JSON.parse(params['fax'])
-		p "================================================================"
-		p request.headers.each {|header| puts header}
-		p "================================================================"
-		p response.headers.each {|header| puts header}
+		p "============================================================"
+		p phaxio_signature = response.headers.HTTP_X_PHAXIO_SIGNATURE
+		p phaxio_signature2 = response.headers.HTTP_X_PHAXIO_SIGNATURE2
+		p "============================================================"
+
 		email_addresses = UserEmail.find_by(fax_tag: @fax['tags']['sender_email_fax_tag']).email_address
 
     if @fax["status"] == "success"
@@ -48,14 +42,10 @@ class MailgunFaxesController < ApplicationController
 	end
 
 	def mailgun(files = [])
-		p "================================================================"
-		p params
-		p "================================================================"
-		@fax = JSON.parse(params['fax'])
-		p "================================================================"
-		p request.headers.each {|header| puts header}
-		p "================================================================"
-		p response.headers.each {|header| puts header}
+		p "============================================================"
+		phaxio_signature = response.headers.HTTP_X_PHAXIO_SIGNATURE
+		phaxio_signature2 = response.headers.HTTP_X_PHAXIO_SIGNATURE2
+		p "============================================================"
     # return [400, "Must include a sender"] if !params['from']					# Make this send a fail email
     # return [400, "Must include a recipient"] if !params['recipient']	# Make this send a fail email
     sender = Mail::AddressList.new(params['from']).addresses.first.address
