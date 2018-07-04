fc7640df4b9557261241dac73a536f1bd6283410
Matt Mayerle <mwmayerle@gmail.com>
2018-07-04 10:55:03 -0500

    callback token debugging

--- app/controllers/mailgun_faxes_controller.rb
+++ app/controllers/mailgun_faxes_controller.rb
@@ -10,7 +10,7 @@ class MailgunFaxesController < ApplicationController
 		p "================================================================"
 		p "================================================================"
 		p "================================================================"
-		p request.headers.HTTP_X_PHAXIO_SIGNATURE
+		p request.headers["HTTP_X_PHAXIO_SIGNATURE"]
 		@fax = JSON.parse(params['fax'])
     recipient_number = Phonelib.parse(@fax['to_number']).e164
     fax_number = FaxNumber.find_by(fax_number: recipient_number)
