edb2d7a412d2e39335bdae961a0081aa96188def
Matt Mayerle <mwmayerle@gmail.com>
2018-07-10 12:43:10 -0500

    added sstyling tweaks, pulled user_emails editing out of the fax_number controller for ease of reading and clarity

--- app/assets/javascripts/faxes.js
+++ app/assets/javascripts/faxes.js
@@ -1,2 +1,44 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+	addAdditionalFileInput();
+	// closeFileDropWindow();
+	adjustAttachmentCount();
+});
+
+function adjustAttachmentCount() {
+	let files = $(".file-counter");
+	let filesArray = $.makeArray(files).reverse(); // .reverse() doesn't work on a jQuery object
+	filesArray.forEach(function(fileLabel) {
+		$(fileLabel).text("File: " + (filesArray.indexOf(fileLabel) + 1))
+	});
+}
+
+function addAdditionalFileInput() {
+	$("#send-another").on("click", function(event) {
+		if ($(".file-attachments :file").val() === "") {
+			FileDrop.removeEventListener('dragenter', handleDragEnter);
+	    FileDrop.removeEventListener('dragover', handleDragOver);
+	    FileDrop.removeEventListener('drop', handleDrop);
+	    FileDrop.removeEventListener('dragleave', handleDragLeave);
+	    FileDrop.registerAll();
+		} else {
+		fileCounter += 1;
+			$("#all-files").prepend(
+				"<div class='form-group file-attachments'>" +
+				"<label class='file-counter'>File " + fileCounter + ":</label>" +
+					"<div class='file-drop' data-target='#faxFile" + fileCounter + "'>" +
+						"<button type='button' class='close' data-dismiss='#faxfile" + fileCounter + "' aria-label='Close'>" + 
+							"<span>&times;</span>" +
+						"</button>" +
+						"<h3>Drag a file onto this box to upload it.</h3>" +
+						"<input id='faxFile" + fileCounter + "' name='files[file" + fileCounter +"]' type='file' required>" +
+					"</div>" +
+					"<div class='faxFileProgress' hidden='hidden'></div>" +
+				"</div>"
+			);
+		}
+		adjustAttachmentCount();
+	});
+}
--- app/controllers/fax_numbers_controller.rb
+++ app/controllers/fax_numbers_controller.rb
@@ -12,7 +12,6 @@ class FaxNumbersController < ApplicationController
 	def edit
 		@user = User.new
 		@clients = Client.all if is_admin?
-		@fax_number.unused_client_emails = FaxNumber.get_unused_client_emails(@fax_number) if @fax_number.client
 	end
 
 	def update
@@ -26,11 +25,6 @@ class FaxNumbersController < ApplicationController
 				original_client_user_email_ids = {}
 				original_client.user_emails.each { |user_email| original_client_user_email_ids[user_email.id] = 'on' }
 				remove_user_email_associations(original_client_user_email_ids, @fax_number)
-			end
-
-			unless params[:user_emails].nil?
-				add_user_email_associations(user_email_association_params[:to_add], @fax_number) if params[:user_emails][:to_add]
-				remove_user_email_associations(user_email_association_params[:to_remove], @fax_number) if params[:user_emails][:to_remove]
 			end
 
 			flash[:notice] = "Changes successfully saved."
@@ -54,14 +48,6 @@ class FaxNumbersController < ApplicationController
 
 		def client_manager_fax_number_params
 			params.require(:fax_number).permit(:id, :fax_number_display_label)
-		end
-
-		def user_email_association_params
-			params.require(:user_emails).permit(:to_add => {}, :to_remove => {})
-		end
-
-		def add_user_email_associations(param_input, fax_number_object) # equivalent of @fax_number << UserEmail
-			param_input.keys.each { |user_email_object_id| fax_number_object.user_emails << UserEmail.find(user_email_object_id.to_i) }
 		end
 
 		def remove_user_email_associations(param_input, fax_number_object)
