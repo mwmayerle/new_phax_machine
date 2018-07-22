6a1b4d55f4b2255ed406b1d11c0bf3009884f678
Matt Mayerle <mwmayerle@gmail.com>
2018-07-22 11:44:17 -0500

    Verify phaxio callback debugging

--- app/controllers/mailgun_faxes_controller.rb
+++ app/controllers/mailgun_faxes_controller.rb
@@ -87,9 +87,7 @@ class MailgunFaxesController < ApplicationController
 	  end
 
 	  def strong_params
-	  	params[:fax] = ActionController::Parameters.new({ :fax => JSON.parse(params[:fax])})
-	  	p "=================="
-	  	p params
+	  	params = ActionController::Parameters.new({ :fax => JSON.parse(params[:fax])})
 	  	params.require(:fax).permit(JSON.parse(params[:fax]))
 	  end
 end
