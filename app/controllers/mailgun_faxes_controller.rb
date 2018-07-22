8d998e3f3e1e97760ab691339b8763c95353506e
Matt Mayerle <mwmayerle@gmail.com>
2018-07-22 15:13:32 -0500

    testing renamed params

--- app/controllers/mailgun_faxes_controller.rb
+++ app/controllers/mailgun_faxes_controller.rb
@@ -6,7 +6,7 @@ class MailgunFaxesController < ApplicationController
   private_constant :DIGEST
 
 	def fax_received
-		@fax = strong_phaxio_params
+		@fax = JSON.parse(params[:fax])
     recipient_number = Phonelib.parse(@fax['to_number']).e164
     fax_number = FaxNumber.find_by(fax_number: recipient_number)
 
@@ -24,7 +24,7 @@ class MailgunFaxesController < ApplicationController
 	end
 
 	def fax_sent
-		@fax = strong_phaxio_params
+		@fax = JSON.parse(params[:fax])
 		email_addresses = User.find_by(fax_tag: @fax['tags']['sender_email_fax_tag']).email
 
     if @fax["status"] == "success"
@@ -60,109 +60,3 @@ class MailgunFaxesController < ApplicationController
 			MailgunMailer.failed_email_to_fax_email(sender, sent_fax_object).deliver_now
 		end
 	end
-
-	private
-		def verify_phaxio_callback
-			p params
-			Fax.set_phaxio_creds
-	    signature = request.env['HTTP_X_PHAXIO_SIGNATURE']
-	    file_params = params['file']
-	    p "*****************************************************************************"
-	    url = request.url
-	    phaxio_params = strong_phaxio_params.to_h
-	    if valid_signature?(signature, url, phaxio_params, file_params)
-	    	puts "IT WORKED!!!!!!!!!!!!!!!"
-	    	render(status: 200)
-	    else
-	    	puts "IT BROKE!!!!!!!!!!!!!!!!!"
-	    	render(status: 422)
-	    end
-	  end
-
-	  def callback_params
-	    phaxio_params.select do |key, _value|
-	      %w(success is_test direction fax metadata message).include?(key)
-	    end
-	  end
-
-	  def strong_phaxio_params
-	  	# Create new params from the existing params and then filter them due to the JSON component returned from the Phaxio API
-	  	new_params = ActionController::Parameters.new({:fax => JSON.parse(params[:fax])})
-	  	new_params.require(:fax).permit(	  		
-	  		:id,
-	  		:direction,
-	  		:num_pages,
-	  		:status,
-	  		:is_test,
-	  		:caller_id,
-	  		:caller_name,
-	  		:from_number,
-	  		:caller_name,
-	  		:cost,
-	  		:to_number,
-	  		:error_id,
-	  		:error_type,
-	  		:error_message,
-	  		:file,
-	  		{ :barcodes => [] },
-	  		{ :tags => [
-	  			:sender_organization_fax_tag,
-	  			:sender_email_fax_tag]
-	  		},
-	  		{ :recipients => [
-	  				:phone_number,
-	  				:status,
-	  				:error_type,
-	  				:error_id,
-	  				:error_message,
-	  				:to_number
-	  			]
-	  		},
-	  	)
-	  end
-
-	  ####################################################
-
-	  def valid_signature? signature, url, params, files = []
-      check_signature = generate_check_signature url, params, files
-      check_signature == signature
-    end
-
-	  def generate_check_signature url, params, files = []
-      params_string = generate_params_string(params)
-      files_string = generate_files_string(files)
-      callback_data = "#{url}#{params_string}#{files_string}"
-      OpenSSL::HMAC.hexdigest(DIGEST, callback_token, callback_data)
-    end
-
-    def callback_token
-      Phaxio.callback_token or raise(Phaxio::Error::PhaxioError, 'No callback token has been set')
-    end
-
-    def generate_params_string(params)
-      sorted_params = params.keys.sort_by { |key, _value| key }
-      params_strings = sorted_params.map { |key, value| "#{key}#{value}" }
-      params_strings.join
-    end
-
-    def generate_files_string(files)
-      files_array = files_to_array(files).reject(&:nil?)
-      p "11111111111111111111111111111111"
-      p files_array
-      p "22222222222222222222222222222222"
-      ##############
-      sorted_files = files_array.sort_by { |file| file.name }
-      ##############
-      p sorted_files
-      files_strings = sorted_files.map { |file| generate_file_string(file) }
-      files_strings.join
-    end
-
-    def files_to_array(files)
-      files.is_a?(Array) ? files : [files]
-    end
-
-    def generate_file_string(file)
-      file.name + DIGEST.hexdigest(file.tempfile.read)
-    end
-end
