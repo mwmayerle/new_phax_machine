c085807facda69e12cf26ed0ab1f8eaacaf031dc
Matt Mayerle <mwmayerle@gmail.com>
2018-07-30 17:24:21 -0500

    heroku debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.raise_delivery_errors = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
