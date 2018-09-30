f41dec16977e9b792a63342b0b9d1da27c9d0652
Matt Mayerle <mwmayerle@gmail.com>
2018-09-30 11:15:31 -0500

    Continuing to flesh out and edit documentation. Added a few 'if fax_numbers.present?' blocks to forbid users that are not linked to a fax number from sending faxes

--- app/controllers/mailgun_faxes_controller.rb
+++ app/controllers/mailgun_faxes_controller.rb
@@ -10,26 +10,31 @@ class MailgunFaxesController < ApplicationController
     	fax_num_email_obj.user.email
     end
 
-    fax_from = @fax['from_number']
-  	fax_file_name = params['file'].original_filename
-    fax_file_contents = params['file'].read
+    if email_addresses.present? # requires the User to be linked to a fax number
+	    fax_from = @fax['from_number']
+	  	fax_file_name = params['file'].original_filename
+	    fax_file_contents = params['file'].read
 
-    email_subject = "Fax received from #{fax_from}"
-		MailgunMailer.fax_email(email_addresses, email_subject, @fax, fax_file_name, fax_file_contents).deliver_now
+	    email_subject = "Fax received from #{fax_from}"
+			MailgunMailer.fax_email(email_addresses, email_subject, @fax, fax_file_name, fax_file_contents).deliver_now
+		end
 	end
 
 	def fax_sent
 		@fax = JSON.parse(params['fax'])
-		email_addresses = User.find_by(fax_tag: @fax['tags']['sender_email_fax_tag']).email
+		email_addresses = User.includes(:fax_numbers).find_by(fax_tag: @fax['tags']['sender_email_fax_tag']).email
 
-    if @fax["status"] == "success"
-    	email_subject = "Your fax was sent successfully"
-    else
-    	@fax["most_common_error"] = Fax.most_common_error(@fax)
-    	email_subject = "Your fax failed because: #{@fax["most_common_error"]}"
-    end
+		if email_addresses.fax_numbers.present? # requires the User to be linked to a fax number
+	    if @fax["status"] == "success"
+	    	email_subject = "Your fax was sent successfully"
+	    else
+	    	@fax["most_common_error"] = Fax.most_common_error(@fax)
+	    	email_subject = "Your fax failed because: #{@fax["most_common_error"]}"
+	    end
 
-		MailgunMailer.fax_email(email_addresses, email_subject, @fax).deliver_now
+			MailgunMailer.fax_email(email_addresses, email_subject, @fax).deliver_now
+		end
+
 	end
 
 	def mailgun(files = [])
@@ -49,6 +54,7 @@ class MailgunFaxesController < ApplicationController
     end
 
  		sent_fax_object = Fax.create_fax_from_email(sender, params['recipient'], files, user)
+
  		if sent_fax_object.class != String
 			api_response = Fax.get_fax_information(sent_fax_object.id)
 		else
