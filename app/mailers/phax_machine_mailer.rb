5483c4b674453cbab5c25801a33c7c691a25267b
Matt Mayerle <mwmayerle@gmail.com>
2018-12-06 10:10:41 -0600

    Added favicon for a demo, changed default email icon link, added an icon for a demo to the image assets

--- app/mailers/phax_machine_mailer.rb
+++ app/mailers/phax_machine_mailer.rb
@@ -2,13 +2,15 @@ class PhaxMachineMailer < Devise::Mailer
   helper :application
 	include Devise::Mailers::Helpers
   include Devise::Controllers::UrlHelpers 
-  default template_path: 'devise/mailer' 
+  default template_path: 'devise/mailer'
+
+  ALTERNATE_LOGO_PATH = 'https://t3com.com/wp-content/uploads/2016/05/t3comm-process-colors.jpg'.freeze
 
   def manager_welcome_invite(record, token, opts = {})
   	opts[:from] = ENV.fetch('ADMIN_EMAIL')
   	opts[:subject] = "You've been invited to manage #{record.organization.label}."
     @token = token
-    @logo_link = LogoLink.first ? LogoLink.first.logo_url : 'https://assets.voyant.com/logos/voyant-logo.png'
+    @logo_link = LogoLink.first ? LogoLink.first.logo_url : ALTERNATE_LOGO_PATH
     devise_mail(record, :manager_welcome_invite, opts)
   end
 
@@ -16,20 +18,20 @@ class PhaxMachineMailer < Devise::Mailer
   	record.manager ? opts[:from] = record.manager.email : opts[:from] = 'phax_machine@phax_machine.com'
   	opts[:subject] = "You've been invited to Phax Machine."
     @token = token
-    @logo_link = LogoLink.first ? LogoLink.first.logo_url : 'https://assets.voyant.com/logos/voyant-logo.png'
+    @logo_link = LogoLink.first ? LogoLink.first.logo_url : ALTERNATE_LOGO_PATH
     devise_mail(record, :user_welcome_invite, opts)
   end
 
   def admin_welcome_invite(record, token, opts = {})
   	opts[:subject] = "You've been invited to be the administer of Phax Machine"
     @token = token
-    @logo_link = LogoLink.first ? LogoLink.first.logo_url : 'https://assets.voyant.com/logos/voyant-logo.png'
+    @logo_link = LogoLink.first ? LogoLink.first.logo_url : ALTERNATE_LOGO_PATH
     devise_mail(record, :admin_welcome_invite, opts)
   end
 
   def reset_password_instructions(record, token, opts = {})
   	@token = token
-  	@logo_link = LogoLink.first ? LogoLink.first.logo_url : 'https://assets.voyant.com/logos/voyant-logo.png'
+  	@logo_link = LogoLink.first ? LogoLink.first.logo_url : ALTERNATE_LOGO_PATH
     devise_mail(record, :reset_password_instructions, opts)
   end
 end
