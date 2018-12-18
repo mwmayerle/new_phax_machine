4ee8b4497078162c0f0715ab7dc40ebd32533593
Matt Mayerle <mwmayerle@gmail.com>
2018-12-18 14:21:10 -0600

    viewing statements

--- app/models/fax_log.rb
+++ app/models/fax_log.rb
@@ -78,6 +78,8 @@ class FaxLog < ApplicationRecord
 							if options[:tag][:sender_organization_fax_tag] && filtered_data != nil
 								filtered_data = filter_faxes_by_org_date(options, filtered_data, organizations[options[:tag][:sender_organization_fax_tag]])
 								# This prevents sent faxes from other organizations from appearing when they shouldn't
+								p "===================================================================================================="
+								p fax_numbers.keys
 								filtered_data = filtered_data.select { |fax_object| fax_numbers.keys.include?(fax_object[:to_number]) }
 							end
 						end
@@ -137,12 +139,6 @@ class FaxLog < ApplicationRecord
 
 			new_data = from_number_data + to_number_data + caller_id_data + recipients_data
 			new_data = new_data.uniq
-
-			# results = []
-			# fax_numbers.each do |fn_key, fn_value|
-
-			# end
-
 			new_data
 		end
 
