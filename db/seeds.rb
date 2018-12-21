2f2331c344897de8f33bd498b40ab5aca7499ce3
Matt Mayerle <mwmayerle@gmail.com>
2018-12-21 13:13:36 -0600

    Added error handling to prevent Fax Numbers for being deleted by a bad API call

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ CSV.foreach('./db/NAME_HERE', {headers: true}) do |row|
   	admin_id: admin.id
   )
 
-  current_fax_number = FaxNumber.find_or_create_by!(fax_number: "#{Phonelib.parse(row[2]).e164}")
+  current_fax_number = FaxNumber.find_or_create_by!(fax_number: "#{Phonelib.parse(row[2]).e164}", org_switched_at: Time.now)
   current_org.fax_numbers << current_fax_number unless current_org.fax_numbers.include?(current_fax_number)
 
   unless row[1].nil?
