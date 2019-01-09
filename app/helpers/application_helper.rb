dc298989ced1a78d269d8a306a05f01de9b3580b
Matt Mayerle <mwmayerle@gmail.com>
2019-01-09 16:04:50 -0600

    clarifying linking a user nag message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
 			flash.now[:alert] = no_caller_id_users.map { |user| user.email }.join(", ").concat(" have no caller ID number and cannot fax. Assign a caller ID number by clicking the #{@organization.label} Users button, and then click edit next to the user's name.")
 		end
 		if unlinked_users.present?
-			message = unlinked_users.map { |user| user.email }.join(", ").concat(" are not linked to a fax number and cannot fax. Click the 'Link/Unlink Users' button on your Dashboard to link these users to a fax number. ")
+			message = unlinked_users.map { |user| user.email }.join(", ").concat(" are not linked to a fax number and cannot fax. Click #{@organization.label} to navigate to its dashboard, and assign a caller ID number by clicking the #{@organization.label} Users button, and then click edit next to the user's name. This process is demonstrated in the Readme in the sidebar. Click the 'Link/Unlink Users' button on your Dashboard to link these users to a fax number. ")
 			# Display both messages or only one
 			flash.now[:alert] ? flash.now[:alert] += "\n".concat(message) : flash.now[:alert] = message
 		end
