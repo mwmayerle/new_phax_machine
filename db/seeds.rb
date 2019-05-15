db511edcc3a692eaa2b7d6097ee17097b5136560
Matt Mayerle <mwmayerle@gmail.com>
2019-05-14 22:30:37 -0500

    askjd

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,13 @@ lol = CSV.parse(file, {headers: true}) do |row|
 	fn = FaxNumber.find_or_create_by!(fax_number: Phonelib.parse(row[0]).e164)
 	org = Organization.find_or_create_by!(admin_id: admin.id, label: row[1])
 	org.fax_numbers << fn
-	fn.reload!
-	org.reload!
+	fn.reload
+	org.reload
 	row[3..-1].each do |email|
-		p email
 		unless email.blank?
 			fart = User.new(email: email, organization_id: org.id, caller_id_number: fn.fax_number, user_permission_attributes: {permission: UserPermission::USER})
 			fart.save!
-			fart.reload!
+			fart.reload
 			ufn = UserFaxNumber.create!(user_id: fart.id, fax_number_id: fn.id)
 		end
 	end
