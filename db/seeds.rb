def4391c2c5dc38874abb331c4d9fd4f011660d8
Matt Mayerle <mwmayerle@gmail.com>
2019-05-14 22:01:11 -0500

    fixing csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'csv'
 # Create the admin
 admin = User.create!(
-	email: 'pontiacaztek@outlook.com',
+	email: 'mwmayerle@gmail.com',
 	user_permission_attributes: { permission: UserPermission::ADMIN }
 )
 
@@ -10,13 +10,17 @@ lol = CSV.parse(file, {headers: true}) do |row|
 	fn = FaxNumber.find_or_create_by!(fax_number: Phonelib.parse(row[0]).e164)
 	org = Organization.find_or_create_by!(admin_id: admin.id, label: row[1])
 	org.fax_numbers << fn
-	
+	fn.reload!
+	org.reload!
 	row[3..-1].each do |email|
+		p email
 		unless email.blank?
 			fart = User.new(email: email, organization_id: org.id, caller_id_number: fn.fax_number, user_permission_attributes: {permission: UserPermission::USER})
 			fart.save!
-			fart.fax_numbers.push(fn) unless fart.fax_numbers.include?(fn)
+			fart.reload!
+			ufn = UserFaxNumber.create!(user_id: fart.id, fax_number_id: fn.id)
 		end
 	end
 end
-# FaxNumber.format_and_retrieve_fax_numbers_from_api
+
+FaxNumber.format_and_retrieve_fax_numbers_from_api
